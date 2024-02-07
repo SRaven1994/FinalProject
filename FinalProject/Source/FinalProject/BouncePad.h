c753e1fb1f7442a7cb750c3ce3ca96cd1f00691f
SRaven1994 <92119448+SRaven1994@users.noreply.github.com>
2024-02-07 16:28:48 +0000

    c

--- FinalProject/Source/FinalProject/BouncePad.h
+++ FinalProject/Source/FinalProject/BouncePad.h
@@ -24,11 +24,11 @@ public:
 	virtual void Tick(float DeltaTime) override;
 
 	// Create collision functions
-	UFUNCTION()
-	virtual void OnOverlapBegin(class UPrimitiveComponent* newComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	// UFUNCTION()
+	// virtual void OnOverlapBegin(class UPrimitiveComponent* newComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
-	UFUNCTION()
-	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	// UFUNCTION()
+	// virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
 
 
 };
